b445f6936f2aab5a12ae2daa2a5d5357e6f1e895
Marina.Zakharina <machm@mail.ru>
2026-07-24 00:31:08 +0300

    Migrations for table payment & config for connection

---
+++ app/core/config.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_DB}"
+        )
+
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env"
+    )
+
+
+settings = Settings()
+
+if __name__ == '__main__':
+    print(settings.database_url)
---
+++ check_db.py
@@ -0,0 +1,19 @@
+import asyncio
+import asyncpg
+
+
+async def main():
+    conn = await asyncpg.connect(
+        user="postgres",
+        password="postgres",
+        database="payment_db",
+        host="localhost",
+        port=5432,
+    )
+
+    print("connected")
+
+    await conn.close()
+
+
+asyncio.run(main())
